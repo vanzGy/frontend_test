dfc40562b3d50927bd008737d649438f03465725
vanzent <vanzent.lee@gmail.com>
2024-04-08 20:21:43 +0800

    remove url link

--- src/helpers/openWeatherQuery.ts
+++ src/helpers/openWeatherQuery.ts
@@ -1,8 +1,7 @@
 export default async function openWeatherQuery(cityName: String, countryName: String) {
     // A global api call function.
    
-    const openWeatherApi = `http://api.openweathermap.org/data/2.5/weather?q=${cityName},${countryName}&units=metric`;
-    return fetch(openWeatherApi)
+    return fetch('test')
                   .then((res) => res.json())
                   .catch((err)=>{throw Error(err)})
         
