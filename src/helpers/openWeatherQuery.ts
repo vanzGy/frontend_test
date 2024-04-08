a6fb3c257a73a474092c8fabf73fad112fee294c
vanzent <vanzent.lee@gmail.com>
2024-04-08 20:15:17 +0800

    remove api kehy

--- src/helpers/openWeatherQuery.ts
+++ src/helpers/openWeatherQuery.ts
@@ -1,7 +1,7 @@
 export default async function openWeatherQuery(cityName: String, countryName: String) {
     // A global api call function.
-    const api = 'a47dbbf97c6a91c4ca699a474c295f48';
-    const openWeatherApi = `http://api.openweathermap.org/data/2.5/weather?q=${cityName},${countryName}&units=metric&appid=${api}`;
+   
+    const openWeatherApi = `http://api.openweathermap.org/data/2.5/weather?q=${cityName},${countryName}&units=metric`;
     return fetch(openWeatherApi)
                   .then((res) => res.json())
                   .catch((err)=>{throw Error(err)})
